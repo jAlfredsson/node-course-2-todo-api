c58582dfb8d3ff4da51f84563c22b4b4819e0dc5
jAlfredsson <john@killnoise.com>
2018-09-23 18:11:40 +0200

    Add DELETE /users/me/token tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -277,4 +277,20 @@ describe('POST /users/login', () => {
       .expect(400)
       .end(done);
   });
+});
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0)
+          done();
+        }).catch((e) => done(e));
+      })
+      .end(done)
+  })
 })
