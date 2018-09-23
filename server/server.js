cb49205f33e7094c10c8cd61ce76fd5bca61cb3b
jAlfredsson <john@killnoise.com>
2018-09-23 16:32:24 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express')
 const bodyParser = require('body-parser')
 const {ObjectID} = require('mongodb')
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
@@ -104,6 +105,25 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+//   User.findOne({email: body.email}).then((user) => {
+//     if(!user){return res.status(400).send()}
+//     bcrypt.compare(body.password,  user.password, (err, rett) => {
+//       if(err){return res.status(400).send()}
+//       if(rett){res.status(200).send(user)}
+//     })
+// })
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
